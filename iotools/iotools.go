11428feb17f0e1299b45435c266e651b76b2af41
icwells <smrupp16@gmail.com>
2018-06-07 12:41:14 -0700

    Added writeToCSV to iotools

--- iotools/iotools.go
+++ iotools/iotools.go
@@ -5,6 +5,7 @@ package iotools
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func OpenFile(file string) *os.File {
@@ -19,8 +20,22 @@ func OpenFile(file string) *os.File {
 func CreateFile(file string) *os.File {
 	f, err := os.Create(file)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "[ERROR] Reading %s: %v\n", file, err)
+		fmt.Fprintf(os.Stderr, "[ERROR] Creating %s: %v\n", file, err)
 		os.Exit(1)
 	}
 	return f
+}
+
+func writeToCSV(outfile, header string, results []string) {
+	// Writes slice of slices to file
+	out := CreateFile(outfile)
+	defer out.Close()
+	_, err := out.WriteString(header + "\n")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[ERROR] Writing header to %s: %v\n", outfile, err)
+	}
+	for _, i := range results {
+		// Write comma seperated items to file
+		_, err := out.WriteString(strings.Join(i, ",") + "\n")
+	}
 }
