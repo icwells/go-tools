5a196387cfca31d8e0ea9ea96f9338e5b3af8117
icwells <smrupp16@gmail.com>
2020-04-03 14:51:18 -0700

    Finished fractiontests

--- fraction/fraction.go
+++ fraction/fraction.go
@@ -4,154 +4,88 @@ package fraction
 
 import (
 	"fmt"
+	"math"
 )
 
 type Fraction struct {
-	Denominator	int
-	Numerator	int
+	Denominator int
+	Numerator   int
 }
 
 // Returns new fraction with n as numerator and d as denominator. Reduces fraction if possible.
-// Returns an empty fraction (0/1) if n if less than 0 or d is less than 1.
+// Returns an empty fraction (0/1) if d is less than 1.
 func NewFraction(n, d int) *Fraction {
 	f := new(Fraction)
-	d.Denominator = 1
-	if n >= 0 && d > 0 {
-		f.Numerator = n
-		f.Denomniator = d
+	f.Denominator = 1
+	if d > 0 {
+		gcd := f.greatestCommonDenominator(n, d)
+		f.Numerator = int(float64(n) / gcd)
+		f.Denominator = int(float64(d) / gcd)
 	}
 	return f
 }
 
 // greatestCommonDenominator calculates the gratest common denominator of n and d using Euchlid's algorithm.
-func (f *Fraction) greatestCommonDenominator(n, d int) float64{
-	for a != b {
+func (f *Fraction) greatestCommonDenominator(n, d int) float64 {
+	n = int(math.Abs(float64(n)))
+	for n != d {
 		if n > d {
 			n -= d
 		} else {
-			d-= n
+			d -= n
+		}
+		if n <= 0 || d <= 0 {
+			n = 1
+			break
 		}
 	}
 	return float64(n)
 }
 
-class Fraction {
-    private:
-        // Calculates the greates common divisor with
-        // Euclid's algorithm
-        // both arguments have to be positive
-        long long gcd(long long a, long long b) {
-            while (a != b) {
-                if (a > b) {
-                    a -= b;
-                } else {
-                    b -= a;
-                }
-            }
-            return a;
-        }
-
-    public:
-        long long numerator, denominator;
-
-        Fraction() {
-            numerator = 0;
-            denominator = 1;
-        }
-
-        Fraction(long long n, long long d) {
-            if (d==0) {
-                cerr << "Denominator may not be 0." << endl;
-                exit(0);
-            } else if (n == 0) {
-                numerator = 0;
-                denominator = 1;
-            } else {
-                int sign = 1;
-                if (n < 0) {
-                    sign *= -1;
-                    n *= -1;
-                }
-                if (d < 0) {
-                    sign *= -1;
-                    d *= -1;
-                }
-
-                long long tmp = gcd(n, d);
-                numerator = n/tmp*sign;
-                denominator = d/tmp;
-            }
-        }
-
-        operator int() {return (numerator)/denominator;}
-        operator float() {return ((float)numerator)/denominator;}
-        operator double() {return ((double)numerator)/denominator;}
-};
-
-Fraction operator+(const Fraction& lhs, const Fraction& rhs) {
-    Fraction tmp(lhs.numerator*rhs.denominator
-                +rhs.numerator*lhs.denominator,
-                lhs.denominator*rhs.denominator);
-    return tmp;
+// String returns fraction formatted as a string.
+func (f *Fraction) String() string {
+	return fmt.Sprintf("%d/%d", f.Numerator, f.Denominator)
 }
 
-Fraction operator+=(Fraction& lhs, const Fraction& rhs) {
-    Fraction tmp(lhs.numerator*rhs.denominator
-                +rhs.numerator*lhs.denominator,
-                lhs.denominator*rhs.denominator);
-    lhs = tmp;
-    return lhs;
+// Float returns numerator/denominator.
+func (f *Fraction) Float() float64 {
+	return float64(f.Numerator) / float64(f.Denominator)
 }
 
-Fraction operator-(const Fraction& lhs, const Fraction& rhs) {
-    Fraction tmp(lhs.numerator*rhs.denominator
-                -rhs.numerator*lhs.denominator,
-                lhs.denominator*rhs.denominator);
-    return tmp;
+// Equals returns true if two fractions are equal.
+func (f *Fraction) Equals(v *Fraction) bool {
+	if f.Numerator == v.Numerator && f.Denominator == v.Denominator {
+		return true
+	}
+	return false
 }
 
-Fraction operator-=(Fraction& lhs, const Fraction& rhs) {
-    Fraction tmp(lhs.numerator*rhs.denominator
-                -rhs.numerator*lhs.denominator,
-                lhs.denominator*rhs.denominator);
-    lhs = tmp;
-    return lhs;
+// Less returns true if f is less than v.
+func (f *Fraction) Less(v *Fraction) bool {
+	if f.Numerator/f.Denominator < v.Numerator/v.Denominator {
+		return true
+	}
+	return false
 }
 
-Fraction operator*(const Fraction& lhs, const Fraction& rhs) {
-    Fraction tmp(lhs.numerator*rhs.numerator,
-               lhs.denominator*rhs.denominator);
-    return tmp;
+// Add adds a fraction to f.
+func (f *Fraction) Add(v *Fraction) *Fraction {
+	n := f.Numerator*v.Denominator + v.Numerator*f.Denominator
+	return NewFraction(n, f.Denominator*v.Denominator)
 }
 
-Fraction operator*=(Fraction& lhs, const Fraction& rhs) {
-    Fraction tmp(lhs.numerator*rhs.numerator,
-               lhs.denominator*rhs.denominator);
-    lhs = tmp;
-    return lhs;
+// Subtract subtracts a fraction from f.
+func (f *Fraction) Subtract(v *Fraction) *Fraction {
+	n := f.Numerator*v.Denominator - v.Numerator*f.Denominator
+	return NewFraction(n, f.Denominator*v.Denominator)
 }
 
-Fraction operator*(int lhs, const Fraction& rhs) {
-    Fraction tmp(lhs*rhs.numerator,rhs.denominator);
-    return tmp;
+// Multiply multiplies v times f.
+func (f *Fraction) Multiply(v *Fraction) *Fraction {
+	return NewFraction(f.Numerator*v.Numerator, f.Denominator*v.Denominator)
 }
 
-Fraction operator*(const Fraction& rhs, int lhs) {
-    Fraction tmp(lhs*rhs.numerator,rhs.denominator);
-    return tmp;
-}
-
-Fraction operator/(const Fraction& lhs, const Fraction& rhs) {
-    Fraction tmp(lhs.numerator*rhs.denominator,
-                 lhs.denominator*rhs.numerator);
-    return tmp;
-}
-
-std::ostream& operator<<(std::ostream &strm, const Fraction &a) {
-    if (a.denominator == 1) {
-        strm << a.numerator;
-    } else {
-        strm << a.numerator << "/" << a.denominator;
-    }
-    return strm;
+// Divide divides f by v.
+func (f *Fraction) Divide(v *Fraction) *Fraction {
+	return NewFraction(f.Numerator*v.Denominator, f.Denominator*v.Numerator)
 }
---
+++ fraction/fraction_test.go
@@ -0,0 +1,43 @@
+package fraction
+
+import (
+	"testing"
+)
+
+func TestFraction(t *testing.T) {
+	a := NewFraction(1, 3)
+	b := NewFraction(3, 28)
+	c := NewFraction(1, 3)
+	if a.String() != "1/3" {
+		t.Errorf("Actual fraction %s does not equal 1/3.", a.String())
+	} else if b.String() != "3/28" {
+		t.Errorf("Actual fraction %s does not equal 3/28.", b.String())
+	} else if a.Equals(b) {
+		t.Errorf("%s does not equal %s.", a.String(), b.String())
+	} else if !a.Equals(c) {
+		t.Errorf("%s equals %s.", a.String(), c.String())
+	} else if a.Less(b) {
+		t.Errorf("%s is greater than %s.", a.String(), c.String())
+	} else {
+		c = a.Add(b)
+		if c.String() != "37/84" {
+			t.Errorf("%s + %s does not equal 37/84.", a.String(), b.String())
+		}
+		c = a.Subtract(b)
+		if c.String() != "19/84" {
+			t.Errorf("%s - %s does not equal 19/84.", a.String(), b.String())
+		}
+		c = a.Multiply(b)
+		if c.String() != "1/28" {
+			t.Errorf("%s * %s does not equal 1/28.", a.String(), b.String())
+		}
+		c = a.Divide(b)
+		if c.String() != "28/9" {
+			t.Errorf("%s / %s does not equal 28/9.", a.String(), b.String())
+		}
+		c = a.Multiply(NewFraction(-1, 1))
+		if c.String() != "-1/3" {
+			t.Errorf("%s * %s does not equal -1/3.", a.String(), b.String())
+		}
+	}
+}
