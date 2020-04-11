4214a0b54c64ecf61f2b23730c5d217fe62106ca
icwells <smrupp16@gmail.com>
2020-04-11 16:15:36 -0700

    Added Copy and FromFloat functions, and functions for arithemtic with integers and floating point numbers

--- fraction/fraction.go
+++ fraction/fraction.go
@@ -5,6 +5,8 @@ package fraction
 import (
 	"fmt"
 	"math"
+	"strconv"
+	"strings"
 )
 
 // Fraction struct stores the numerator and denominator of a fraction as integers.
@@ -43,6 +45,19 @@ func (f *Fraction) greatestCommonDenominator(n, d int) float64 {
 	return float64(n)
 }
 
+// FromFloat converts a float64 to a fraction.
+func FromFloat(n float64) *Fraction {
+	d := 1
+	// Get number of digits after decimal
+	l := len(strings.Split(strconv.FormatFloat(n, 'f', 64, -1), ".")[1])
+	return NewFraction(int(n*float64(l)), d*l)
+}
+
+// Copy returns a deep copy of f.
+func (f *Fraction) Copy(v *Fraction) *Fraction {
+	return NewFraction(f.Numerator, f.Denominator)
+}
+
 // String returns fraction formatted as a string.
 func (f *Fraction) String() string {
 	return fmt.Sprintf("%d/%d", f.Numerator, f.Denominator)
@@ -69,10 +84,22 @@ func (f *Fraction) Less(v *Fraction) bool {
 	return false
 }
 
+//---------------Arithmetic Operations----------------------------------------
+
 // Add adds a fraction to f.
 func (f *Fraction) Add(v *Fraction) *Fraction {
 	n := f.Numerator*v.Denominator + v.Numerator*f.Denominator
 	return NewFraction(n, f.Denominator*v.Denominator)
+}
+
+// AddInt adds n/d to f.
+func (f *Fraction) AddInt(n, d int) *Fraction {
+	return f.Add(NewFraction(n, d))
+}
+
+// AddFloat adds a floating point number to f.
+func (f *Fraction) AddFloat(n float64) *Fraction {
+	return FromFloat(f.Float() + n)
 }
 
 // Subtract subtracts a fraction from f.
@@ -81,12 +108,46 @@ func (f *Fraction) Subtract(v *Fraction) *Fraction {
 	return NewFraction(n, f.Denominator*v.Denominator)
 }
 
+// SubtractInt subtracts n/d from f. Returns the absolute value of the result if abs is true.
+func (f *Fraction) SubtractInt(n, d int, abs bool) *Fraction {
+	return f.SubtractFloat(float64(n) / float64(d), abs)
+}
+
+// SubtractFloat subtracts a floating point number from f. Returns the absolute value of the result if abs is true.
+func (f *Fraction) SubtractFloat(n float64, abs bool) *Fraction {
+	v := f.Float() - n
+	if abs {
+		v = math.Abs(v)
+	}
+	return FromFloat(v)
+}
+
 // Multiply multiplies v times f.
 func (f *Fraction) Multiply(v *Fraction) *Fraction {
 	return NewFraction(f.Numerator*v.Numerator, f.Denominator*v.Denominator)
 }
 
+// MultiplyInt multiplies f by n/d.
+func (f *Fraction) MultiplyInt(n, d int) *Fraction {
+	return f.Multiply(NewFraction(n, d))
+}
+
+// MultiplyFloat multiplies f by a floating point number.
+func (f *Fraction) MultiplyFloat(n float64) *Fraction {
+	return FromFloat(f.Float() * n)
+}
+
 // Divide divides f by v.
 func (f *Fraction) Divide(v *Fraction) *Fraction {
 	return NewFraction(f.Numerator*v.Denominator, f.Denominator*v.Numerator)
+}
+
+// DivideInt divides f by n/d.
+func (f *Fraction) DivideInt(n, d int) *Fraction {
+	return f.Divide(NewFraction(n, d))
+}
+
+// DivideFloat divides f by a floating point number.
+func (f *Fraction) DivideFloat(n float64) *Fraction {
+	return FromFloat(f.Float() / n)
 }
